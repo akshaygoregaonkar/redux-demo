6a1e1e88026b99ddd82bf747eb1c37a1c8a9b14b
Akshay Goregaonkar <akshaygoregaonkar@gmail.com>
2021-07-21 11:44:16 +0530

    done

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
     {/* <Dashboard/> */}
-    {/* <TestDashboard/> */}
+    <TestDashboard/>
     {/* <UsingHooks/> */}
-    <FormValidation/>
+    {/* <FormValidation/> */}
     </div>
   );
 }
